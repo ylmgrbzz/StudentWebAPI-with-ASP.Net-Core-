f6f2e2b8d84d13b00401fdc54094ceaf00752451
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-05-23 10:40:06 +0300

    StudenWebAPI with ASP.Net Core

---
+++ StudentWebAPI/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentWebAPI.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.; initial Catalog = lbs; User Id:ylmgrbz; password =ylmgrbz; TrustServerCertificate= True");
+        }
+    }
+}
