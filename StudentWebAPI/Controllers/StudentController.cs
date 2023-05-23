e6aa264bbd5b2a217c016431773f6821d11cd49f
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-05-23 10:51:24 +0300

    StudenWebAPI with ASP.Net Core

---
+++ StudentWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentWebAPI.Models;
+
+namespace StudentWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentDbContext _studentDbContext;
+        public StudentController(StudentDbContext studentDbContext)
+        {
+            _studentDbContext = studentDbContext;
+        }
+
+        [HttpGet]
+        [Route("GetStudent")]
+        public async Task<IEnumerable<Student>> GetStudent()
+        {
+            return await _studentDbContext.Students.ToListAsync();
+        }
+
+        [HttpPost]
+        [Route("AddStudent")]
+        public async Task<ActionResult<Student>> AddStudent(Student student)
+        {
+            _studentDbContext.Students.Add(student);
+            await _studentDbContext.SaveChangesAsync();
+            return student;
+            //return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
+        }
+
+    }
+}
